b2b5c57a358da4109b291b6bc5231464a930b9de
raffaeleav <raff.aviello@gmail.com>
2025-11-20 01:38:32 +0100

    feat: ssbsc upgrades draft

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-import torch
+import os
 
 from ssbsc.swl import tune as tn
 from ssbsc.swl import test as ts
+from ssbsc.core import test as sts
 from ssbsc.helpers import folders as fld
 
 
 if __name__ == "__main__":
-    print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU")
-
     fld.setup()
 
-    # tn.tune_model()
-    ts.test_model()
+    temp_dir = fld.get_temp_dir()
+    results_dir = fld.get_results_dir()
+
+    model = fld.get_file_path(temp_dir, "model.safetensors")
+    swl_results = fld.get_file_path(results_dir, "swl_*.json")
+    ssbsc_results = fld.get_file_path(results_dir, "ssbsc_*.json")
+
+    if os.path.isfile(model): 
+        tn.tune_model()
+    
+    if os.path.isfile(swl_results): 
+        ts.test_model()
+
+    if os.path.isfile(ssbsc_results): 
+        sts.test_model()
---
+++ ssbsc/core/__init__.py
@@ -0,0 +1,14 @@
+# dataset parameters
+MAX_BYTES = 64
+SEGMENTS = 8
+
+# awgn channel parameters
+SNR_DB_LIST = [-2.0, -1.2, -0.6, -0.2, 0.0, 0.2, 0.4, 0.8, 1.5, 2.0]
+
+# training parameters (batch size is set to 32 because of my 8GB VRAM)
+NUM_EPOCHS = 8
+BATCH_SIZE = 32
+LEARNING_RATE = 3e-5
+LOG_STEPS = 100
+WARMUP_STEPS = 500
+MAX_SEQ_LEN = 64
